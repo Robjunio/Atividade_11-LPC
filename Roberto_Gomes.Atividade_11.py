5bc7ba43af1045359f5b9864fc8f5b117767b995
Roberto Gomes <rjrg.snf20@uea.edu.br>
2021-06-30 20:34:31 -0400

    Personagem com movimentação e flip

--- Roberto_Gomes.Atividade_11.py
+++ Roberto_Gomes.Atividade_11.py
@@ -4,34 +4,61 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__()
         self.is_animating = False
-        self.sprites = []
-        self.sprites.append(pygame.image.load("Assets/mario_andando1.png"))
-        self.sprites.append(pygame.image.load("Assets/mario_andando2.png"))
-        self.sprites.append(pygame.image.load("Assets/mario_andando3.png"))
-        self.sprites.append(pygame.image.load("Assets/mario_andando4.png"))
+        self.jumping = False
+        self.sprites_walk = []
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.vel_x = 0
+        self.vel_y = 0
+        self.flip = False
+        
+        # Walking animation
+        self.sprites_walk.append(pygame.image.load("Assets/mario_andando1.png"))
+        self.sprites_walk.append(pygame.image.load("Assets/mario_andando2.png"))
+        self.sprites_walk.append(pygame.image.load("Assets/mario_andando3.png"))
+        self.sprites_walk.append(pygame.image.load("Assets/mario_andando4.png"))
+
+        # Jumping animation
+        self.sprites_jump = []
+        self.sprites_jump.append(pygame.image.load("Assets/mario_andando1.png"))
+        self.sprites_jump.append(pygame.image.load("Assets/mario_pulando1.png"))
+        self.sprites_jump.append(pygame.image.load("Assets/mario_pulando2.png"))
+        
         self.current_sprite = 0
-        self.image = self.sprites[self.current_sprite]
+        self.image = self.sprites_walk[self.current_sprite]
 
         self.rect = self.image.get_rect()
-        self.rect.topleft = [pos_x, pos_y]
-
-
-    def animate(self):
-        self.is_animating = True
+        self.rect.topleft = [self.pos_x, self.pos_y]
         
     def update(self, speed):
-        if self.is_animating == True:
+        if anim == True:
             self.current_sprite += speed
             
-            if self.current_sprite >= len(self.sprites):
+            if self.current_sprite >= len(self.sprites_walk):
                 self.current_sprite = 0
-                self.is_animating = False
 
-            self.image = self.sprites[int(self.current_sprite)]
+            if not self.flip:
+                self.image = self.sprites_walk[int(self.current_sprite)]
+            else:
+                self.image = pygame.transform.flip(self.sprites_walk[int(self.current_sprite)], True, False)
 
-    
+
+    def jump(self):
+        self.pos = -20
+        
+    def move(self, flip):
+        if not self.jumping:
+            if flip:
+                self.vel_x -= 3
+                self.flip = True
+            else:
+                self.vel_x += 3
+                self.flip = False
+                
 pygame.init()
 clock = pygame.time.Clock()
+
+anim = False
 
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 400
@@ -39,7 +66,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Sprite Animation")
 
 moving_sprites = pygame.sprite.Group()
-player = Player(200,200)
+player = Player(200, 200)
 moving_sprites.add(player)
 
 while True:
@@ -48,10 +75,40 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            player.animate()
+            if event.key == pygame.K_SPACE:
+                player.jump()
+            if event.key == pygame.K_RIGHT:
+                player.move(False)
+                anim = True
+            if event.key == pygame.K_LEFT:
+                player.move(True)
+                anim = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                player.vel_y = 0
+                anim = False
+                if player.current_sprite != 0:
+                    player.current_sprite = 0
+                    player.image = player.sprites_walk[int(player.current_sprite)]
+                    
+            if event.key == pygame.K_RIGHT:
+                player.vel_x = 0
+                anim = False
+                if player.current_sprite != 0:
+                    player.current_sprite = 0
+                    player.image = player.sprites_walk[int(player.current_sprite)]
+            if event.key == pygame.K_LEFT:
+                player.vel_x = 0
+                anim = False
+                if player.current_sprite != 0:
+                    player.current_sprite = 0
+                    player.image = pygame.transform.flip(player.sprites_walk[int(player.current_sprite)], True, False)
 
+                    
+    player.pos_x += player.vel_x
+    player.pos_y += player.vel_y
     screen.fill((0, 0, 0))
-    moving_sprites.draw(screen)
+    screen.blit(player.image, (player.pos_x, player.pos_y))
     moving_sprites.update(0.3)
     pygame.display.flip()
     clock.tick(60)
