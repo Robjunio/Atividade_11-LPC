10165ecc91a1b5cbbe2322bf85dbd239eb00c75b
Roberto Gomes <rjrg.snf20@uea.edu.br>
2021-07-02 19:33:27 -0400

    Main character full animated

--- Roberto_Gomes.Atividade_11.py
+++ Roberto_Gomes.Atividade_11.py
@@ -31,34 +31,60 @@ class Player(pygame.sprite.Sprite):
         self.rect.topleft = [self.pos_x, self.pos_y]
         
     def update(self, speed):
-        if anim == True:
-            self.current_sprite += speed
+        if not jump:
+            if anim == True:
+                self.current_sprite += speed
             
-            if self.current_sprite >= len(self.sprites_walk):
-                self.current_sprite = 0
+                if self.current_sprite >= len(self.sprites_walk):
+                    self.current_sprite = 0
 
-            if not self.flip:
-                self.image = self.sprites_walk[int(self.current_sprite)]
-            else:
-                self.image = pygame.transform.flip(self.sprites_walk[int(self.current_sprite)], True, False)
+                if not self.flip:
+                    self.image = self.sprites_walk[int(self.current_sprite)]
+                else:
+                    self.image = pygame.transform.flip(self.sprites_walk[int(self.current_sprite)], True, False)
+        else:
+             if anim == True:
+                self.current_sprite += speed
+            
+                if self.current_sprite >= len(self.sprites_jump):
+                    self.current_sprite = 0
+                    
+                if not self.flip:
+                    self.image = self.sprites_jump[int(self.current_sprite)]
+                else:
+                    self.image = pygame.transform.flip(self.sprites_jump[int(self.current_sprite)], True, False)
+        
 
+                
+    # Função para detectar se o player está no ar
+    def falling(self):
+        self.pos_y += 6
+            
 
+            
     def jump(self):
-        self.pos = -20
+        self.pos_y -= 15
+        
+        
         
     def move(self, flip):
         if not self.jumping:
+            # Detecta se o player virou para assim virar o seu sprite
             if flip:
-                self.vel_x -= 3
+                self.vel_x = -3
                 self.flip = True
             else:
-                self.vel_x += 3
+                self.vel_x = 3
                 self.flip = False
+
                 
 pygame.init()
 clock = pygame.time.Clock()
 
+# Boleano para mostrar que a animação está rodando 
 anim = False
+pos = 0
+jump = False
 
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 400
@@ -76,40 +102,62 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                player.jump()
+                jump = True
+                anim = True
+                
             if event.key == pygame.K_RIGHT:
                 player.move(False)
                 anim = True
+                
+                
             if event.key == pygame.K_LEFT:
                 player.move(True)
                 anim = True
+                
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE:
-                player.vel_y = 0
-                anim = False
-                if player.current_sprite != 0:
-                    player.current_sprite = 0
-                    player.image = player.sprites_walk[int(player.current_sprite)]
-                    
             if event.key == pygame.K_RIGHT:
-                player.vel_x = 0
                 anim = False
-                if player.current_sprite != 0:
-                    player.current_sprite = 0
-                    player.image = player.sprites_walk[int(player.current_sprite)]
+                player.vel_x = 0
+                
             if event.key == pygame.K_LEFT:
-                player.vel_x = 0
                 anim = False
-                if player.current_sprite != 0:
-                    player.current_sprite = 0
-                    player.image = pygame.transform.flip(player.sprites_walk[int(player.current_sprite)], True, False)
-
-                    
+                player.vel_x = 0
+    
+    # Evita que o player fique no ar           
+                   
     player.pos_x += player.vel_x
-    player.pos_y += player.vel_y
+
+    if player.pos_y < 100:
+        jump = False
+        anim = False
+        
+    if jump:
+        player.jump()
+        player.jumping = True
+    else:
+        player.jumping = False
+        
+        
+    if player.pos_y < 200 and player.jumping == False: 
+        player.falling()
+    elif player.pos_y > 200:
+        player.pos_y = 200
+    
+    
     screen.fill((0, 0, 0))
+
+    
+    if not anim and player.current_sprite != 0:
+        player.current_sprite = 0
+        if player.flip:
+            player.image = pygame.transform.flip(player.sprites_walk[int(player.current_sprite)], True, False)
+        else:
+            player.image = player.sprites_walk[int(player.current_sprite)]
+    else:
+        moving_sprites.update(0.3)
+
+        
     screen.blit(player.image, (player.pos_x, player.pos_y))
-    moving_sprites.update(0.3)
     pygame.display.flip()
     clock.tick(60)
     
